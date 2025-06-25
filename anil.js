8c5fff8a27e0f9b306b91d3363a4c5cbf5c63b09
BANOTH ANILKUMAR <banothanilkumar65@gmail.com>
2025-06-25 12:44:01 +0530

    Add files via upload

---
+++ anil.js
@@ -0,0 +1,2 @@
+document.title = "DOM in javascript";
+document.getElementsById("header").innerHTML = "hello,world";
---
+++ dom.js
@@ -0,0 +1,4 @@
+document.title = "DOM in Javascript";
+document.getElementById("header").innerHTML = "Hello, World!";
+document.getElementById("para").style.color = "blue";
+document.getElementById("para").style.fontsize = "30px";
