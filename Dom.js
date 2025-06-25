77cdcd5e50c2c7a945f7c5c43a3922ec2daa3bce
BANOTH ANILKUMAR <banothanilkumar65@gmail.com>
2025-06-25 15:13:12 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,14 @@
+
+Document.title="Dom in javaScript";
+document.body.style.backgroundColor="green";
+document.getElementById("header").innerHTML="Hello ,World!";
+document.getElementById("para").style.color="green";
+document.getElementById("para").style.fontSize="30px";
+document.getElementById("mr").style.color="blue";
+document.getElementById("mr").style.fontSize="30px";
+document.getElementById("k").style.color="red";
+document.getElementById("k").style.fontSize="30px";
+document.getElementById("n").style.color="brown";
+document.getElementById("n").style.fontSize="30px";
+document.getElementById("g").style.color="yellow";
+document.getElementById("g").style.fontSize="30px";
